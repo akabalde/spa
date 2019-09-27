b7b2e9f42e50a4ab5ccfa206cf556234d4f35a6b
DiegoVM <diegobalderrama93@gmail.com>
2019-09-26 21:39:44 -0300

    Cambio de nombre en forms

--- WFA_Spa/FRM_Inicio.cs
+++ WFA_Spa/FRM_Inicio.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WFA_Spa
 {
-    public partial class Form1 : Form
+    public partial class FRM_Inicio : Form
     {
-        public Form1()
+        public FRM_Inicio()
         {
             InitializeComponent();
         }
---
+++ WFA_Spa/FRM_Paciente_Alta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_Spa
+{
+    public partial class FRM_Paciente_Alta : Form
+    {
+        public FRM_Paciente_Alta()
+        {
+            InitializeComponent();
+        }
+    }
+}
