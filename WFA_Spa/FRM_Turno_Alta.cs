936e33b0b6eb48a1e0a9db865f79171b9d607568
Diego <diegobalderrama93@gmail.com>
2019-10-03 21:31:44 -0300

    Added FRM_Turno_Alta.cs

---
+++ WFA_Spa/FRM_Turno_Alta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_Spa
+{
+    public partial class FRM_Turno_Alta : Form
+    {
+        public FRM_Turno_Alta()
+        {
+            InitializeComponent();
+        }
+    }
+}
