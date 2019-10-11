423fefe846cf7c710ecd5456d017b14bc45aa473
DiegoVM <diegobalderrama93@gmail.com>
2019-10-10 21:36:54 -0300

    UI changes

--- WFA_Spa/FRM_Inicio.Designer.cs
+++ WFA_Spa/FRM_Inicio.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRM_Inicio));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.profesionalesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -65,11 +66,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(30, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.Size = new System.Drawing.Size(128, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Proximos turnos";
+            this.label1.Text = "PRÓXIMOS TURNOS";
             // 
             // dataGridView1
             // 
@@ -81,20 +83,20 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(394, 41);
+            this.button1.Location = new System.Drawing.Point(386, 36);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(83, 30);
             this.button1.TabIndex = 3;
-            this.button1.Text = "Nuevo turno";
+            this.button1.Text = "Nuevo Turno";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(313, 41);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(336, 36);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(44, 30);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Imprimir";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // FRM_Inicio
