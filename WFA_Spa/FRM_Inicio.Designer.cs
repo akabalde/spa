5aade4873fd9f51c9caa10f46450ec982791e137
diegobalde <diegobalderrama93@gmail.com>
2019-10-10 22:27:17 -0300

    Added -Tratamientos- to menuStrip

--- WFA_Spa/FRM_Inicio.Designer.cs
+++ WFA_Spa/FRM_Inicio.Designer.cs
@@ -36,56 +36,64 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.tratamientosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.clienteToolStripMenuItem,
-            this.profesionalesToolStripMenuItem});
+            this.profesionalesToolStripMenuItem,
+            this.tratamientosToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(498, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(747, 35);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // clienteToolStripMenuItem
             // 
             this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
-            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(89, 29);
             this.clienteToolStripMenuItem.Text = "Clientes";
             // 
             // profesionalesToolStripMenuItem
             // 
             this.profesionalesToolStripMenuItem.Name = "profesionalesToolStripMenuItem";
-            this.profesionalesToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.profesionalesToolStripMenuItem.Size = new System.Drawing.Size(133, 29);
             this.profesionalesToolStripMenuItem.Text = "Profesionales";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(30, 46);
+            this.label1.Location = new System.Drawing.Point(45, 71);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.Size = new System.Drawing.Size(188, 20);
             this.label1.TabIndex = 1;
             this.label1.Text = "PRÓXIMOS TURNOS";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 72);
+            this.dataGridView1.Location = new System.Drawing.Point(50, 111);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(436, 215);
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(654, 331);
             this.dataGridView1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(386, 36);
+            this.button1.Location = new System.Drawing.Point(579, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 30);
+            this.button1.Size = new System.Drawing.Size(124, 46);
             this.button1.TabIndex = 3;
             this.button1.Text = "Nuevo Turno";
             this.button1.UseVisualStyleBackColor = true;
@@ -93,23 +101,31 @@
             // button2
             // 
             this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
-            this.button2.Location = new System.Drawing.Point(336, 36);
+            this.button2.Location = new System.Drawing.Point(504, 55);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(44, 30);
+            this.button2.Size = new System.Drawing.Size(66, 46);
             this.button2.TabIndex = 4;
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // tratamientosToolStripMenuItem
+            // 
+            this.tratamientosToolStripMenuItem.Name = "tratamientosToolStripMenuItem";
+            this.tratamientosToolStripMenuItem.Size = new System.Drawing.Size(129, 29);
+            this.tratamientosToolStripMenuItem.Text = "Tratamientos";
+            // 
             // FRM_Inicio
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 311);
+            this.ClientSize = new System.Drawing.Size(747, 478);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FRM_Inicio";
             this.Text = "Spa Manager";
             this.menuStrip1.ResumeLayout(false);
@@ -129,6 +145,7 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem tratamientosToolStripMenuItem;
     }
 }
 
